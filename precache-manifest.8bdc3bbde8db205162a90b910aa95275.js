f8551503afc25b4560cf7e042885ae532002d587
olegStacevich <oleg295832611@gmail.com>
2021-08-10 14:04:54 +0200

    Updates

---
+++ precache-manifest.8bdc3bbde8db205162a90b910aa95275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cbbb1574b02dfacba1b2bda2376e927",
+    "url": "/todolist-ts/index.html"
+  },
+  {
+    "revision": "dd4c616d3ba836cbb40c",
+    "url": "/todolist-ts/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "b853e9c1599992e97f8f",
+    "url": "/todolist-ts/static/js/2.1f608c14.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist-ts/static/js/2.1f608c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4c616d3ba836cbb40c",
+    "url": "/todolist-ts/static/js/main.73c80f19.chunk.js"
+  },
+  {
+    "revision": "b7c7c0b009241d8831c6",
+    "url": "/todolist-ts/static/js/runtime-main.8e0caec2.js"
+  }
+]);
